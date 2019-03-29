4ef3f17d9ea04c97f7107f69aac1dde653d2d751
TomasHurtado <33264896+TomasHurtado@users.noreply.github.com>
2019-03-28 22:05:31 -0300

    Add files via upload

--- Clase_04/clase4/main.c
+++ Clase_04/clase4/main.c
@@ -5,9 +5,15 @@
 int main()
 {
     int memoria;
+    int ret;
     //prueba();
    // printf("Hello world!\n");
-   utn_getnumber(&memoria,100,0,"ingrese_nuemero","error");
+   ret = utn_getnumber(&memoria,100,0,"ingrese_nuemero","error\n",3);
    printf("%d",memoria);
+
+   if(ret==1)
+   {
+    printf("fracasaste en intentos");
+   }
     return 0;
 }
